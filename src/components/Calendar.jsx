670e04d1d23c2728c9071e9884a9e13d91a09aad
Kshitij Ingale <kshitijingale2@gmail.com>
2023-02-07 18:41:50 +0530

    Icons Imported

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,10 +3,10 @@ import ContestCard from "./ContestCard";
 function Calendar() {
 
     return (
-        <div className="h-[600px] w-[450px] p-[20px] border-solid border-4 border-blue-500 rounded-[12px]">
+        <div className="h-[600px] w-[450px] p-[20px] border-solid border-4 border-blue-500 rounded-[12px] overflow-hidden overflow-y-auto">
             <div className="flex w-[100%] justify-between items-center">
                 <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-transparent rounded-[6px] py-[8px] shadow-md">Platform ↓</button>
-                <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-blue-500 rounded-[6px] py-[8px] shadow-md">Today</button>
+                <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-blue-500 rounded-[6px] py-[8px] shadow-md">In 24 Hours</button>
                 <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-transparent rounded-[6px] py-[8px] shadow-md">Upcoming</button>
             </div>
             <ContestCard />
--- src/components/ContestCard.jsx
+++ src/components/ContestCard.jsx
@@ -4,7 +4,7 @@ function ContestCard() {
     return (
         <div className='h-[150px] mt-[16px] bg-[#171717] rounded-[6px] shadow-md flex' >
             <div className='self-center  px-[20px]'>
-                <img className='w-[45px]' src={"https://clist.by/imagefit/static_resize/64x64/img/resources/atcoder_jp.png"} />
+                <img className='w-[45px]' src={require("../assets/icons/HackerRank.png")} />
             </div>
             <div className='self-center pr-[20px] overflow-hidden'>
                 <a src={"https://leetcode.com/contest/biweekly-contest-97"} className='cursor-pointer'>
