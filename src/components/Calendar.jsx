06386ba68f738ad09034b498ae0ff5037cc77687
Kshitij Ingale <kshitijingale2@gmail.com>
2023-02-08 20:04:33 +0530

    Data fetched from Kontest API

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,16 +1,47 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import ContestCard from "./ContestCard";
 
 function Calendar() {
+    const [in24hr, setIn24hr] = useState(true)
+    const [list, setList] = useState([])
+
+    async function getData() {
+        const res = await (axios.get('https://kontests.net/api/v1/all'));
+        const contestList = res.data;
+        setList(contestList);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
         <div className="h-[600px] w-[450px] p-[20px] border-solid border-4 border-blue-500 rounded-[12px] overflow-hidden overflow-y-auto">
             <div className="flex w-[100%] justify-between items-center">
                 <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-transparent rounded-[6px] py-[8px] shadow-md">Platform ↓</button>
-                <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-blue-500 rounded-[6px] py-[8px] shadow-md">In 24 Hours</button>
-                <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-transparent rounded-[6px] py-[8px] shadow-md">Upcoming</button>
+                <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-blue-500 rounded-[6px] py-[8px] shadow-md" onClick={() => setIn24hr(true)
+                }>In 24 Hours</button>
+                <button className="bg-[#171717] border px-[15px] text-blue-500 border-solid border-2 border-transparent rounded-[6px] py-[8px] shadow-md" onClick={() => setIn24hr(false)
+                }>Upcoming</button>
             </div>
-            <ContestCard />
-            <ContestCard />
+            {
+                in24hr ? (
+                    list.map((contest) => {
+                        if (contest.in_24_hours === 'Yes') {
+                            return <ContestCard cardData={contest} />
+                        }
+                        return;
+                    })
+                ) : (
+                    list.map((contest) => {
+                        if (contest.in_24_hours === 'No') {
+                            return <ContestCard cardData={contest} />
+                        }
+                        return;
+                    })
+                )
+            }
         </div>
 
     )
--- src/components/ContestCard.jsx
+++ src/components/ContestCard.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
 
-function ContestCard() {
+function ContestCard({ cardData }) {
+
+    const { name, start_time, duration, url, site } = cardData;
+    const startInfo = start_time.split('T');
+    const startDate = startInfo[0].substring(0, 10);
+    let startTime = startInfo[1].substring(0, 8);
+    // start_time is diffrently formatted for CodeChef
+    if (site === 'CodeChef') {
+        startTime = startInfo[0].substring(11, 19);
+    }
+
+    // duration conversion in hours, minutes , second format
+    function convertTime(sec) {
+        if (sec > 86400) {
+            return 'Not Available';
+        }
+        var hours = Math.floor(sec / 3600);
+        (hours >= 1) ? sec = sec - (hours * 3600) : hours = '00';
+        var min = Math.floor(sec / 60);
+        (min >= 1) ? sec = sec - (min * 60) : min = '00';
+        (sec < 1) ? sec = '00' : void 0;
+
+        (min.toString().length === 1) ? min = '0' + min : void 0;
+        (sec.toString().length === 1) ? sec = '0' + sec : void 0;
+
+        return hours + 'hours ' + min + 'mins ' + sec + 's';
+    }
+
+
     return (
         <div className='h-[150px] mt-[16px] bg-[#171717] rounded-[6px] shadow-md flex' >
             <div className='self-center  px-[20px]'>
-                <img className='w-[45px]' src={require("../assets/icons/HackerRank.png")} />
+                <img className='w-[45px]' alt='icon' src={require(`../assets/icons/${site}.png`)} />
             </div>
             <div className='self-center pr-[20px] overflow-hidden'>
-                <a src={"https://leetcode.com/contest/biweekly-contest-97"} className='cursor-pointer'>
-                    <h1 className='text-blue-500 text-lg mb-[10px]'>Biweekly Contest</h1>
+                <a href={`${url}`} className='cursor-pointer'>
+                    <h1 className='text-blue-500 text-lg mb-[10px]'>{name}</h1>
                     <p className='text-[#fff] text-sm'>
-                        Starting Date : { }
+                        Starting Date : {startDate}
                         <br />
-                        Starting Time : { }
+                        Starting Time : {startTime}
                         <br />
-                        Duration : { }
+                        Duration : {convertTime(duration)}
+
                     </p>
                 </a>
             </div>
